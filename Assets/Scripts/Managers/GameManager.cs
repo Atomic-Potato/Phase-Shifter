11ff747d1ff8fa640d5d444cc0b07a6f415892e8
Atomic Potato <medyanmehiddine@gmail.com>
2024-05-03 14:21:38 +0300

    Game Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : Singleton<GameManager>
+{
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+#endif
+    }
+
+}
