1f0425ef083182dc952282ba594f7009a370875e
Atomic Potato <medyanmehiddine@gmail.com>
2024-05-04 11:21:37 +0300

    parallax

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    [SerializeField, Min(0)] float _parallaxMultiplier = 1;
+    Vector2 _startPosition;
+    Transform _camera;
+
+    void Start()
+    {
+        _camera = Camera.main.transform;    
+        _startPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(
+            _startPosition.x + (_camera.position.x * _parallaxMultiplier),
+            _startPosition.y + (_camera.position.y * _parallaxMultiplier),
+            transform.position.z
+        );
+    }
+}
